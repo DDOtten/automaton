df191d90eabddc35f57d7c09e8ec9086e800d109
DDOtten <daniel@otten.co>
2018-02-05 21:05:29 +0100

    turing

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,30 @@
 extern crate automaton;
 
 use automaton::{Automaton, Deter, NonDeter};
+use automaton::turing::{State, Direction};
 
-fn main() {}
+fn main() {
+    let turing = automaton::turing::TuringMachine {
+        initial_state: 'o',
+        transitions: vec![
+            (('o', None), (State::Going('p'), None, Direction::Right)),
+            (('p', Some('a')), (State::Going('q'), Some('a'), Direction::Right)),
+            (('p', Some('b')), (State::Going('s'), Some('b'), Direction::Right)),
+            (('q', Some('a')), (State::Going('q'), Some('a'), Direction::Right)),
+            (('q', Some('b')), (State::Going('r'), Some('b'), Direction::Right)),
+            (('r', Some('a')), (State::Going('r'), Some('a'), Direction::Right)),
+            (('r', Some('b')), (State::Going('r'), Some('b'), Direction::Right)),
+            (('s', Some('a')), (State::Going('t'), Some('a'), Direction::Right)),
+            (('s', Some('b')), (State::Going('s'), Some('b'), Direction::Right)),
+            (('t', Some('a')), (State::Going('q'), Some('a'), Direction::Right)),
+            (('t', Some('b')), (State::Going('r'), Some('b'), Direction::Right)),
+            (('t', None), (State::Accepted, None, Direction::Right)),
+            (('r', None), (State::Accepted, None, Direction::Right)),
+        ].into_iter().collect(),
+    };
+
+    println!("{:?}", turing.accepts("aba".chars().map(|c| Some(c))));
+    println!("{:?}", turing.accepts("abaa".chars().map(|c| Some(c))));
+    println!("{:?}", turing.accepts("ba".chars().map(|c| Some(c))));
+    println!("{:?}", turing.accepts("aaadba".chars().map(|c| Some(c))));
+}
--- src/turing/mod.rs
+++ src/turing/mod.rs
@@ -5,7 +5,7 @@ use Automaton;
 
 #[derive(Debug, Clone)]
 pub enum State<S> {
-    State(S),
+    Going(S),
     Accepted,
     Rejected,
 }
@@ -13,8 +13,8 @@ pub enum State<S> {
 #[derive(Debug, Clone)]
 pub enum Direction {
     Left,
-    Right,
     Stay,
+    Right,
 }
 
 #[derive(Debug, Clone)]
@@ -25,4 +25,53 @@ where
 {
     pub initial_state: S,
     pub transitions: Map<(S, Option<T>), (State<S>, Option<T>, Direction)>,
+}
+
+impl<S, T> Automaton for TuringMachine<S, T>
+where
+    S: Clone + Eq + Hash,
+    T: Clone + Eq + Hash,
+{
+    type Alphabet = Option<T>;
+
+    fn accepts<I>(&self, input: I) -> bool
+    where
+        I: Iterator<Item = Option<T>>,
+    {
+        let mut tape = Vec::with_capacity(input.size_hint().0 + 1);
+        tape.push(None);
+        tape.extend(input);
+
+        let mut state = self.initial_state.clone();
+        let mut i = 0;
+
+        loop {
+            if let Some((new_state, new_symbol, direction)) =
+                self.transitions.get(&(state, tape[i].clone()))
+            {
+                match new_state {
+                    State::Going(new_state) => state = new_state.clone(),
+                    State::Accepted => return true,
+                    State::Rejected => return false,
+                }
+
+                tape[i] = new_symbol.clone();
+
+                match direction {
+                    Direction::Left => if i > 0 {
+                        i -= 1
+                    },
+                    Direction::Stay => {},
+                    Direction::Right => {
+                        i += 1;
+                        if i == tape.len() {
+                            tape.push(None)
+                        }
+                    },
+                }
+            } else {
+                return false;
+            }
+        }
+    }
 }
